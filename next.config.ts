361ca132a8c069fc096e96125894febabac86162
Yogesh <yogeshchaudhry274@gmail.com>
2025-10-14 23:55:39 +0530

    update code

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */  
-  srcDir: "src",
-  experimental: {
-    typedRoutes: true
-  },
+  /* config options here */
   devIndicators: false
 };
 
